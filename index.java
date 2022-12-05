b884dd1812e4298acd4af05f8b81842d5584d068
Ananyadas267 <119876427+Ananyadas267@users.noreply.github.com>
2022-12-05 21:48:43 +0600

    Add files via upload

---
+++ index.java
@@ -0,0 +1,23 @@
+function ret() {
+  var x = document.getElementById("return");
+  var y = document.getElementById("oneway");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "block";
+    y.style.display = "none";
+  }
+}
+
+function oneWay() {
+  var x = document.getElementById("oneway");
+  var y = document.getElementById("return");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "block";
+    y.style.display = "none";
+  }
+}
